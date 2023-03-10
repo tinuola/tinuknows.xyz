e42471b261abf14b2c54b0cb183a160a621dbf91
Tinuola <tinuola@users.noreply.github.com>
2023-03-10 03:16:36 +0000

    Adds files

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.createServer(function(req, res){
+	res.write("On my way to being a full stack engineer--maybe!");
+	res.end();
+}).listen(3000);
+
+console.log('Server started on port 3000');
+
